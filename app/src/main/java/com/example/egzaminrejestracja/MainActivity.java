419cd11c53ef338422f369dbcf45537312d714f7
mikolajpioro <mytocpioro@gmail.com>
2024-06-15 18:48:03 +0200

    current project

--- app/src/main/java/com/example/egzaminrejestracja/MainActivity.java
+++ app/src/main/java/com/example/egzaminrejestracja/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.egzaminrejestracja;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    EditText editPass1;
+    EditText editPass2;
+    TextView textWitaj;
+    Button button;
+    String haslo;
+    String powtorzHaslo;
+    String witaj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editText = (EditText) findViewById(R.id.textPodajEmail);
+        editPass1 = (EditText) findViewById(R.id.textPodajHaslo);
+        editPass2 = (EditText) findViewById(R.id.textPowtorzHaslo);
+        textWitaj = (TextView) findViewById(R.id.textViewWitaj);
+        button = (Button) findViewById(R.id.buttonZatwierdz);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                haslo = String.valueOf(editPass1.getText());
+                powtorzHaslo = String.valueOf(editPass2.getText());
+                witaj = String.valueOf(editText.getText());
+                if(haslo.equals(powtorzHaslo) && witaj.contains("@")){
+                    textWitaj.setText("Witaj " + witaj);
+                }
+                else{
+                    textWitaj.setText("Nieprawidłowe hasło lub e-mail");
+                }
+            }
         });
+
     }
 }
